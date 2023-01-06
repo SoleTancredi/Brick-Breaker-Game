4d764aefaf830bcf052cf78ddb9423c054c64b80
Tancredi M.S. <tancredi620@gmail.com>
2023-01-06 15:38:56 -0300

    draw

--- src/main/java/com/ts/brickbreakergame/MapGenerator.java
+++ src/main/java/com/ts/brickbreakergame/MapGenerator.java
@@ -45,6 +45,11 @@ public class MapGenerator {
         }
     }
     
+    public void setBrickValue(int value, int row, int col){
+        map[row][col] = value;
+        
+    }
+    
     
     
     
